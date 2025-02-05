c6f6b4a60c22fdd5c566549fa3ef5d7a51073dd9
BANDARU T S NAGA VENKATA PRUDHVI RAM <bandaru.prudhviram@bigbasket.com>
2025-02-05 16:01:31 +0530

    Update movie.go

--- models/movie.go
+++ models/movie.go
@@ -2,6 +2,6 @@ package models
 
 type Movie struct {
 	ID          int    `json:"id"`
-	Movie_Name        string `json:"movie_name"`
+	Movie_Name  string `json:"movie_name"`
 	Description string `json:"description"`
 }
