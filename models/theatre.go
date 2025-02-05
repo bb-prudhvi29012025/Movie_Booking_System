56bfb27371e24c9d165c9072b1c119ad1c46f577
BANDARU T S NAGA VENKATA PRUDHVI RAM <bandaru.prudhviram@bigbasket.com>
2025-02-05 16:04:18 +0530

    Update theatre.go

--- models/theatre.go
+++ models/theatre.go
@@ -3,14 +3,14 @@ package models
 import "time"
 
 type Theatre struct {
-	ID          int       `json:"id"`
-	Theatre_Name       string    `json:"theatre_name"`
-	TotalSeats  int       `json:"total_seats"`
-	SeatsBooked int       `json:"seats_booked"`
-	SeatsVacant int       `json:"seats_vacant"`
-	Total_Rooms      int       `json:"total_rooms"`
-	CreatedBy   string    `json:"created_by"`
-	CreatedOn   time.Time `json:"created_on"`
-	UpdatedBy   string    `json:"updated_by"`
-	UpdatedOn   time.Time `json:"updated_on"`
+	ID            int       `json:"id"`
+	Theatre_Name  string    `json:"theatre_name"`
+	TotalSeats    int       `json:"total_seats"`
+	SeatsBooked   int       `json:"seats_booked"`
+	SeatsVacant   int       `json:"seats_vacant"`
+	Total_Rooms   int       `json:"total_rooms"`
+	CreatedBy     string    `json:"created_by"`
+	CreatedOn     time.Time `json:"created_on"`
+	UpdatedBy     string    `json:"updated_by"`
+	UpdatedOn     time.Time `json:"updated_on"`
 }
