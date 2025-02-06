03681eaad0d011b2e9eca9f2b69360487f1b841c
BANDARU T S NAGA VENKATA PRUDHVI RAM <bandaru.prudhviram@bigbasket.com>
2025-02-06 10:13:36 +0530

    Update booking.go

--- models/booking.go
+++ models/booking.go
@@ -4,6 +4,7 @@ type BookingRequest struct {
     TheatreName string `json:"theatre_name"`
     MovieName   string `json:"movie_name"`
     RoomName    string `json:"room_name,omitempty"`
+    NoOfSeats   int    `json:"no_of_seats"`
 }
 
 type BookingResponse struct {
